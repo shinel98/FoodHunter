30863244196d8a11a8a1cd4e91dcbce673f3f36f
jang-yujin <jyj9747@naver.com>
2023-01-03 21:28:24 +0900

    chore[#7] : 리뷰테이블 생성 완료 및 쿼리 기록

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,1 +1,13 @@
 /** table 관리를 위해 테이블 생성 쿼리를 작성하고 관리하는 파일 **/
+create table review_table
+(
+    reviewId BIGINT(20) NOT NULL AUTO_INCREMENT,
+    storeId BIGINT(20) NOT NULL,
+    userId BIGINT(20) NOT NULL,
+    userName VARCHAR(30) NOT NULL,
+    score INT NOT NULL,
+    photo VARCHAR(200) NULL,
+    regiDate TIMESTAMP NOT NULL,
+    reviewContent VARCHAR(3000) NOT NULL,
+    PRIMARY KEY (reviewId)
+);
