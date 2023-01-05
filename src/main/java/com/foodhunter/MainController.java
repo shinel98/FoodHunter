e57eb352ce2a18eb1631a52345d29d7994d842b9
shinel98 <75024473+shinel98@users.noreply.github.com>
2023-01-05 20:44:32 +0900

    feat[#12] : 메인페이지 html,css 구현 (#13)

---
+++ src/main/java/com/foodhunter/MainController.java
@@ -0,0 +1,23 @@
+package com.foodhunter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+//    @RequestMapping(value="/main", method= RequestMethod.GET)
+//    public ModelAndView to_main(Model model) {
+//        ModelAndView mv = new ModelAndView();
+//        System.out.println("main page loaded");
+//        int views=0;
+//        mv.addObject("views",views);
+//        mv.setViewName("main");
+//
+//        return mv;
+//    }
+}
+
