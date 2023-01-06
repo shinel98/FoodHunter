8b66c72a3db4bbf3de9f41c11b48e1cfb88982dc
jang-yujin <jyj9747@naver.com>
2023-01-06 17:09:06 +0900

    style[#17] : 가게 제보 상세 페이지 UI 구현 완료

--- src/main/java/com/foodhunter/ReportController.java
+++ src/main/java/com/foodhunter/ReportController.java
@@ -9,4 +9,9 @@ public class ReportController {
     public String home() {
         return "report";
     }
+
+    @RequestMapping("/report/category-detail")
+    public String categorySelect(){
+        return "report-detail";
+    }
 }
