fc899c96c08a5f7da1ec318916c7500d2dc6cc4c
Narin Han <lynniepro21@gmail.com>
2023-01-08 21:17:47 +0900

    flag[#6] : 컨트롤러 추가

--- src/main/java/com/foodhunter/ReportController.java
+++ src/main/java/com/foodhunter/ReportController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ReportController {
     @RequestMapping("/report")
-    public String home() {
+    public String report() {
         return "report";
+    }
+
+    @RequestMapping("/category-request")
+    public String categoryRequest() {
+        return "category-request";
     }
 }
