1f3cd609e05b4bb3614866312e77a78a12f45f87
shinel98 <75024473+shinel98@users.noreply.github.com>
2023-01-13 01:40:21 +0900

    Mainpage/#43 (#44)
    * feat[#40]: 닉네임 페이지 구현
    
    * npm install
    
    * feat[#43]:backend 예시구현
    
    * feat[#43]: backend 예시 구현

--- src/main/java/com/foodhunter/MainController.java
+++ src/main/java/com/foodhunter/MainController.java
@@ -1,18 +1,35 @@
 package com.foodhunter;
 
+
+import com.foodhunter.DTO.Store;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.foodhunter.service.StoreService;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
+
 public class MainController {
-
+    @Autowired
+    private StoreService storeService;
     @RequestMapping(value="/main", method= RequestMethod.GET)
-    public String to_main(Model model) {
+    public ModelAndView readStores(Model model) {
+        ModelAndView mv = new ModelAndView();
+        System.out.println("main page loaded");
 
-        return "main";
+        List<Store> storeList = new ArrayList<Store>();
+        storeList = storeService.readStores();
+
+        mv.addObject("storeList", storeList);
+        mv.setViewName("main");
+        return mv;
     }
     @RequestMapping(value="/nickname", method= RequestMethod.GET)
     public String to_nickname(Model model) {
