d0375c822d8214efe271182a90b38fe4947cf1b5
양시준 <sijun0905@gmail.com>
2023-01-15 01:54:53 +0900

    feat[#46] : Implement load list of add category request

--- src/main/java/com/foodhunter/AdminController.java
+++ src/main/java/com/foodhunter/AdminController.java
@@ -1,12 +1,26 @@
 package com.foodhunter;
 
+import com.foodhunter.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.Objects;
 
 @Controller
 public class AdminController {
+    @Autowired
+    CategoryService categoryService;
+
     @RequestMapping("/admin")
-    public String admin(){
+    public String admin(Model model, HttpSession session) {
+        model.addAttribute("categoryRequestList", categoryService.getCategoryListByRequestStatus(1));
+        System.out.println(categoryService.getCategoryListByRequestStatus(1).size());
+
+        // TODO: Checks if the account has administrator privileges
+
         return "admin";
     }
 }
