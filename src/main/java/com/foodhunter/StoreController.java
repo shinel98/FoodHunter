5a56737ec9bcc603b0e53a1cb92e584378269438
jang-yujin <jyj9747@naver.com>
2023-01-18 17:23:07 +0900

    feat[#58]: 방문인증 가게별로 read

--- src/main/java/com/foodhunter/StoreController.java
+++ src/main/java/com/foodhunter/StoreController.java
@@ -2,10 +2,7 @@ package com.foodhunter;
 
 //import com.foodhunter.DAO.ReviewFileUpload;
 import com.foodhunter.DAO.ReviewFileUpload;
-import com.foodhunter.DTO.Category;
-import com.foodhunter.DTO.Favorite;
-import com.foodhunter.DTO.Review;
-import com.foodhunter.DTO.Store;
+import com.foodhunter.DTO.*;
 import com.foodhunter.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,13 +20,17 @@ public class StoreController {
     private final StoreServiceImpl storeService;
 
     private final CategoryServiceImpl categoryService;
+    private final VisitService visitService;
+    private final OpenDayService openDayService;
 
     @Autowired
-    public StoreController(ReviewService reviewService, FavoriteService favoriteService, StoreServiceImpl storeService, CategoryServiceImpl categoryService) {
+    public StoreController(ReviewService reviewService, FavoriteService favoriteService, StoreServiceImpl storeService, CategoryServiceImpl categoryService, VisitService visitService, OpenDayService openDayService) {
         this.reviewService = reviewService;
         this.favoriteService = favoriteService;
         this.storeService = storeService;
         this.categoryService = categoryService;
+        this.visitService = visitService;
+        this.openDayService = openDayService;
     }
 
     /**가게 로드 -> 가게, 리뷰 정보 가져오기**/
@@ -47,9 +48,28 @@ public class StoreController {
         if(currentLike > 0L) {
             model.addAttribute("like", true);
         }
+
+        List<Visit> visitList = visitService.readByStoreId(store.getId());
+        List<StoreMarker> allMarkers = storeService.readMarkerInfo();
+        List<Favorite> favoriteList = favoriteService.readByStoreId(store.getId());
+        StoreMarker storeMarker = new StoreMarker();
+        for(int i=0; i<allMarkers.size(); i++){
+            if(allMarkers.get(i).getStoreId() == store.getId()) {
+                storeMarker = allMarkers.get(i);
+                break;
+            }
+        }
+
+        List<OpenDay> openDayList = openDayService.readByStoreId(store.getId());
+        System.out.println("openDayList size : " + openDayList.size());
+
         model.addAttribute("store", store);
         model.addAttribute("reviews", reviews);
+        model.addAttribute("storeMarker", storeMarker);
         model.addAttribute("categoryList", categoryList);
+        model.addAttribute("favoriteList", favoriteList);
+        model.addAttribute("openDayList", openDayList);
+        model.addAttribute("visitList", visitList);
         model.addAttribute("reviewError", false);
         model.addAttribute("delete", false);
         return "store";
@@ -60,7 +80,7 @@ public class StoreController {
     public String create(HttpServletRequest request, Model model){
         ReviewFileUpload fileUpload = new ReviewFileUpload();
         Review review = fileUpload.uploadPhoto(request);
-
+        System.out.println("storeId: " + review.getStoreId());
         Long result = reviewService.write(review);
         if(result == -1L) { // 에러 발생
             model.addAttribute("reviewError", true);
