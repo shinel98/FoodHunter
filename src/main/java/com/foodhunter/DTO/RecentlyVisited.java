557903a0548eb6665d4cfe8784297415acb9f6fd
양시준 <sijun0905@gmail.com>
2023-01-18 19:06:36 +0900

    feat[#60] : Display recently visited stores list

---
+++ src/main/java/com/foodhunter/DTO/RecentlyVisited.java
@@ -0,0 +1,31 @@
+package com.foodhunter.DTO;
+
+public class RecentlyVisited {
+    private long id;
+    private String name;
+    private long categoryId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+}
--- src/main/java/com/foodhunter/myController.java
+++ src/main/java/com/foodhunter/myController.java
@@ -1,12 +1,40 @@
 package com.foodhunter;
 
+import com.foodhunter.DTO.Category;
+import com.foodhunter.DTO.RecentlyVisited;
+import com.foodhunter.DTO.User;
+import com.foodhunter.service.RecentlyVisitedService;
+import com.foodhunter.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class myController {
+    @Autowired
+    RecentlyVisitedService recentlyVisitedService;
+    @Autowired
+    StoreService storeService;
+
     @RequestMapping(value="/my/recently-visited")
-    public String recentlyVisited(){
+    public String recentlyVisited(HttpSession session, Model model){
+        long userId = ((User) session.getAttribute("user")).getId();
+        System.out.println("userId: " + userId);
+        List<RecentlyVisited> recentlyVisitedList = recentlyVisitedService.getRecentlyVisitedList(userId);
+        List<Category> allCategories = storeService.readAllCategories();
+
+        for (RecentlyVisited recentlyVisited : recentlyVisitedList) {
+            System.out.println(recentlyVisited.getName());
+        }
+
+        model.addAttribute("recentlyVisitedList", recentlyVisitedList);
+        model.addAttribute("allCategories", allCategories);
+
         return "recentlyVisited";
     }
 
